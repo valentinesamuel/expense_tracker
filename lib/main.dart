06cf390dae8498df347ac6e7c220f2e7ce248948
Valentine <valentinesamuel2580@gmail.com>
2021-01-13 03:36:01 +0100

    (FEAT):Improved transaction tile styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -12,6 +13,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        amount: 12.99, date: DateTime.now(), id: 't1', title: 'Clothes'),
+    Transaction(amount: 19.99, date: DateTime.now(), id: 't2', title: 'Food'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +28,7 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Container( 
+            Container(
               width: double.infinity,
               child: Card(
                 child: Text(
@@ -32,7 +39,44 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
-            Card(child: Text('List Of Tx!', style: TextStyle(fontSize: 20),), elevation: 6,)
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            '\$${tx.amount}',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.purple,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(tx.date.toString(),
+                              style: TextStyle(color: Colors.grey))
+                        ],
+                      ),
+                    )
+                  ]),
+                  elevation: 3,
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
