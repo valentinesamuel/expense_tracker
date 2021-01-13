712fed41887726fd03f26310fc27891d1382827c
Valentine <valentinesamuel2580@gmail.com>
2021-01-13 02:48:38 +0100

    (FEAT):Added a transactions model

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,27 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter App'),),
-      
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container( 
+              width: double.infinity,
+              child: Card(
+                child: Text(
+                  'CHART!!',
+                  style: TextStyle(fontSize: 20),
+                ),
+                elevation: 8.0,
+                color: Colors.blue,
+              ),
+            ),
+            Card(child: Text('List Of Tx!', style: TextStyle(fontSize: 20),), elevation: 6,)
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+
+  Transaction({this.amount, this.date, this.id, this.title});
+}
